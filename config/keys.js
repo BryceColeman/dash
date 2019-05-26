51974d25a9dd88d0255fd0a05428a95807fc7279
Bryce Coleman <coleman.bryce@gmail.com>
2019-05-26 06:24:16 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "mysql": {
+    "host": "localhost",
+    "user": "root",
+    "pass": "Jemaco21!!",
+    "db": "db"
+  },
+  "google": {
+    "id": 2,
+    "company": "ACME"
+  }
+}
